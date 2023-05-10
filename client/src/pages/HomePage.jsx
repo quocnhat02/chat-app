7c66f3b455e213749460d122a016fd2fc9a9f0a4
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-10 14:43:11 +0700

    home 1

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,28 @@
+import { Box, Container, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW={'xl'} centerContent>
+      <Box
+        d={'flex'}
+        justifyContent={'center'}
+        alignContent={'center'}
+        p={3}
+        bg={'white'}
+        w={'100%'}
+        m={'40px 0 15px 0'}
+        borderRadius={'lg'}
+        borderWidth={'1px'}
+        textAlign={'center'}
+      >
+        <Text fontSize={'4xl'} color={'black'}>
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box></Box>
+    </Container>
+  );
 };
 
 export default HomePage;
