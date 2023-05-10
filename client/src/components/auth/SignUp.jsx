5e4dfe86f83c97bded544cf3e488a926cdb3dd83
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-10 15:06:21 +0700

    home 5

--- client/src/components/auth/SignUp.jsx
+++ client/src/components/auth/SignUp.jsx
@@ -17,7 +17,13 @@ const SignUp = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [pic, setPic] = useState('');
 
-  const handleClick = () => setShow(!show);
+  const handleShow = () => setShow(!show);
+
+  const postDetails = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <VStack spacing={'5px'} color={'black'}>
@@ -44,19 +50,46 @@ const SignUp = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
           <InputRightElement width={'4.5rem'}>
-            <Button h={'1.75rem'} size={'sm'} onClick={handleClick}>
+            <Button h={'1.75rem'} size={'sm'} onClick={handleShow}>
               {show ? 'hide' : 'show'}
             </Button>
           </InputRightElement>
         </InputGroup>
       </FormControl>
+      <FormControl id='confirm-password' isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? 'text' : 'password'}
+            placeholder='Confirm your password'
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <InputRightElement width={'4.5rem'}>
+            <Button h={'1.75rem'} size={'sm'} onClick={handleShow}>
+              {show ? 'hide' : 'show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
       <FormControl id='email' isRequired>
-        <FormLabel>Email</FormLabel>
+        <FormLabel>Upload your Picture</FormLabel>
         <Input
-          placeholder='Enter your email'
-          onChange={(e) => setEmail(e.target.value)}
+          type='file'
+          p={1.5}
+          accept='image/*'
+          onChange={(e) => postDetails(e.target.files[0])}
         />
       </FormControl>
+
+      <Button
+        colorScheme='blue'
+        width={'100%'}
+        style={{ marginTop: 15 }}
+        onClick={handleSubmit}
+      >
+        Sign Up
+      </Button>
     </VStack>
   );
 };
