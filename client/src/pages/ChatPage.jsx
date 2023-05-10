90b11908c6f24a94fa80e14c3a274053abf8dcf6
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-10 14:12:27 +0700

    render list

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,17 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const ChatPage = () => {
+  const [chats, setChats] = useState([]);
+
   useEffect(() => {
     const fetchChats = async () => {
-      const res = await axios.get('/api/chats');
-      console.log(res.data);
+      const { data } = await axios.get('/api/chats');
+      setChats(data);
     };
 
     fetchChats();
   }, []);
 
-  return <div>ChatPage</div>;
+  return (
+    <div>
+      {chats.map((chat) => (
+        <h2 key={chat._id}>{chat.chatName}</h2>
+      ))}
+    </div>
+  );
 };
 
 export default ChatPage;
