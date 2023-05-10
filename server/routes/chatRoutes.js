d213417cb14ed660608e30801c903dd7650fadf1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-10 21:53:19 +0700

    chat controller 2

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -3,13 +3,14 @@ const protectRoute = require('../middleware/authMiddleware');
 const {
   accessChat,
   fetchChats,
+  createGroupChat,
 } = require('../controller/chatController');
 
 const router = express.Router();
 
 router.route('/').post(protectRoute, accessChat);
 router.route('/').get(protectRoute, fetchChats);
-// router.route('/group').post(protectRoute, createGroupChat);
+router.route('/group').post(protectRoute, createGroupChat);
 // router.route('/rename-group').put(protectRoute, renameGroupChat);
 // router.route('/group-remove').put(protectRoute, removeFromGroupChat);
 // router.route('/group-add').post(protectRoute, addToGroupChat);
