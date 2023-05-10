095cfb31689db927fe464ef9bf51c14eedc51ae8
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-10 14:36:04 +0700

    font

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ChatPage from './pages/ChatPage';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/chats' element={<ChatPage />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
