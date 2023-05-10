8b43e1396574e82cc0c5b277ef5465bcd9618ee8
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-10 16:55:59 +0700

    login

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -6,18 +6,73 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
 
   const handleShow = () => setShow(!show);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: 'Please fill all the fields',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const { data } = await axios.post(
+        '/api/users/login',
+        {
+          email,
+          password,
+        },
+        config
+      );
+      toast({
+        title: 'Login successful',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      setLoading(false);
+      navigate('/chats');
+    } catch (error) {
+      toast({
+        title: 'Error occurred',
+        description: error.response.data.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      setLoading(false);
+    }
   };
 
   return (
@@ -50,6 +105,7 @@ const Login = () => {
         width={'100%'}
         style={{ marginTop: 15 }}
         onClick={handleSubmit}
+        isLoading={loading}
       >
         Login
       </Button>
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 
 router.route('/').post(registerUser);
 
-router.route('/login').get(authUser);
+router.route('/login').post(authUser);
 
 module.exports = router;
