f6a344430e6fad74c45ae10eddb7b7d8073e658b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-14 11:14:31 +0700

    add member and remove member 1

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -15,7 +15,7 @@ router.route('/').post(protectRoute, accessChat);
 router.route('/').get(protectRoute, fetchChats);
 router.route('/group').post(protectRoute, createGroupChat);
 router.route('/group-rename').put(protectRoute, renameGroupChat);
-router.route('/group-add').post(protectRoute, addToGroupChat);
+router.route('/group-add').put(protectRoute, addToGroupChat);
 router.route('/group-remove').put(protectRoute, removeFromGroupChat);
 
 module.exports = router;
